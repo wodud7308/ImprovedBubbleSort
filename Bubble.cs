49ac4301afa005828ea2d9a89e560c3cef87b6ff
wodud7308 <wodud7308@naver.com>
2016-11-08 20:00:44 +0900

    ImprovedBubbleSort
    It was easy, but it's result fell short of my expectations. So, this
    algorithm needs to be improved.

---
+++ Bubble.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace Sort
+{
+    class Bubble
+    {
+        private bool IsSorted;
+        private int RepeatCount;
+        public int SortCount
+        {
+            get { return RepeatCount; }
+        }
+        private void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public void ImprovedBubble(int[] dataArray)
+        {
+            IsSorted = false;
+            RepeatCount = 0;
+            while (!IsSorted)
+            {
+                IsSorted = true;
+                for (int seq = 0; seq < dataArray.Length - 1; seq++)
+                {
+                    if (dataArray[seq] > dataArray[seq + 1])
+                    {
+                        Swap(ref dataArray[seq], ref dataArray[seq + 1]);
+                        IsSorted = false;
+                    }
+                    RepeatCount++;
+                }
+            }
+        }
+
+        public void BubbleSort(int[] dataArray)
+        {
+            RepeatCount = 0;
+            for(int cycle = 0; cycle<dataArray.Length; cycle++)
+            { 
+                for (int seq = 0; seq < dataArray.Length - 1; seq++)
+                {
+                    if (dataArray[seq] > dataArray[seq + 1])
+                    {
+                        Swap(ref dataArray[seq], ref dataArray[seq + 1]);
+                    }
+                    RepeatCount++;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Sort;
+using System;
+namespace MainProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[5];
+            data[0] = 5;
+            data[1] = 4;
+            data[2] = 6;
+            data[3] = 1;
+            data[4] = 2;
+            int[] dataClone = (int[])data.Clone();
+            var bubble = new Bubble();
+
+            bubble.ImprovedBubble(data);
+            Console.WriteLine("RepeatCount : {0}", bubble.SortCount);
+            bubble.BubbleSort(dataClone);
+            Console.WriteLine("RepeatCount : {0}", bubble.SortCount);
+
+            //*** result
+            //RepeatCount : 16
+            //RepeatCount : 20
+        }
+    }
+}
